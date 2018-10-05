f788927494482b75c8dc2e05539ffc5d32aa74ab
Vlad Muradov <vmuradov9273@jh-mba158-11.d214.org>
2018-10-05 11:13:37 -0500

    Initial Commit

---
+++ Commision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commision
+//
+//  Created by Vlad Muradov on 10/5/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
